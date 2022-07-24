a61fa7db5092ebbc27e355655a114e65335b70b1
Conrad Taylor <conradwt@gmail.com>
2022-07-24 15:10:43 -0700

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Conrad Taylor on 7/24/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
