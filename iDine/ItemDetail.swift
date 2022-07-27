e7e1c76555fdb94ca88828902675765a42a5920c
Conrad Taylor <conradwt@gmail.com>
2022-07-26 19:38:39 -0700

    Add initial implementation of order view.

--- iDine/ItemDetail.swift
+++ iDine/ItemDetail.swift
@@ -9,36 +9,45 @@
 import SwiftUI
 
 struct ItemDetail: View {
+  @EnvironmentObject var order: Order
+  
   let item: MenuItem
   
-    var body: some View {
-      VStack {
-        ZStack(alignment: .bottomTrailing) {
-          Image(item.mainImage)
-            .resizable()
-            .scaledToFit()
-          
-          Text("Photo: \(item.photoCredit)")
-            .padding(4)
-            .background(Color.black)
-            .font(.caption)
-            .foregroundColor(.white)
-            .offset(x: -5, y: -5)
-        }
+  var body: some View {
+    VStack {
+      ZStack(alignment: .bottomTrailing) {
+        Image(item.mainImage)
+          .resizable()
+          .scaledToFit()
         
-        Text(item.description)
-          .padding()
-        Spacer()
+        Text("Photo: \(item.photoCredit)")
+          .padding(4)
+          .background(Color.black)
+          .font(.caption)
+          .foregroundColor(.white)
+          .offset(x: -5, y: -5)
       }
-      .navigationTitle(item.name)
-      .navigationBarTitleDisplayMode(.inline)
+      
+      Text(item.description)
+        .padding()
+      
+      Button("Order This") {
+        order.add(item: item)
+      }
+      .font(.headline)
+      
+      Spacer()
     }
+    .navigationTitle(item.name)
+    .navigationBarTitleDisplayMode(.inline)
+  }
 }
 
 struct ItemDetail_Previews: PreviewProvider {
-    static var previews: some View {
-      NavigationView {
-        ItemDetail(item: MenuItem.example)
-      }
+  static var previews: some View {
+    NavigationView {
+      ItemDetail(item: MenuItem.example)
+        .environmentObject(Order())
     }
+  }
 }
---
+++ iDine/OrderView.swift
@@ -0,0 +1,45 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Conrad Taylor on 7/26/22.
+//  Copyright © 2022 Conrad Taylor. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+  @EnvironmentObject var order: Order
+  
+  var body: some View {
+    NavigationView {
+      List {
+        Section {
+          ForEach(order.items) { item in
+            HStack {
+              Text(item.name)
+              Spacer()
+              Text("$\(item.price)")
+            }
+          }
+        }
+        
+        Section {
+          NavigationLink(destination: Text("Check out")) {
+            Text("Place Order")
+          }
+        }
+        
+      }
+      .navigationTitle("Order")
+      .listStyle(InsetGroupedListStyle())
+    }
+  }
+}
+
+struct OrderView_Previews: PreviewProvider {
+  static var previews: some View {
+    OrderView()
+      .environmentObject(Order())
+  }
+}
